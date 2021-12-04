46cc8e8bcceef52d73844eaf478d8f45ebd049cc
FWdarling <1853204@tongji.edu.cn>
2021-12-04 11:26:07 +0800

    feature: 使用element ui的tree展示了目录结构

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+  ></el-tree>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+          children: "children",
+          label: "name"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus() {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+        }).then(({data})=> {
+            console.log('success',data.data)
+            this.menus = data.data
+        })
+    }
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  created() {
+      this.getMenus();
+  }
+};
+</script>
+
+<style scoped>
+</style>
