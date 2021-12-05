4dfcbcc1903024a743673c326072cce926fcaf87
FWdarling <1853204@tongji.edu.cn>
2021-12-05 15:27:26 +0800

    feature: 完成删除单一菜单和添加菜单功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,10 +1,48 @@
 
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -17,33 +55,93 @@ export default {
   data() {
     return {
       menus: [],
+      expandKey: [],
+      dialogVisible: false,
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+      },
       defaultProps: {
-          children: "children",
-          label: "name"
-      }
+        children: "children",
+        label: "name",
+      },
     };
   },
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
-    },
     getMenus() {
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get',
-        }).then(({data})=> {
-            console.log('success',data.data)
-            this.menus = data.data
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("success", data.data);
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+      console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId
+      this.category.catLevel = data.catLevel + 1
+    },
+
+    remove(node, data) {
+      this.$confirm(`是否删除【${data.catId}】菜单？`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          let ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            console.log("success");
+            this.expandKey = [node.parent.data.catId];
+            this.getMenus();
+          });
+          console.log("remove", node, data);
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
         })
-    }
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+
+    addCategory() {
+      console.log("form data", this.category);
+      this.$http({
+      url: this.$http.adornUrl('/product/category/save'),
+      method: 'post',
+      data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => { 
+        this.$message({
+          type: "success",
+          message: "创建成功"
+        })
+        this.dialogVisible = false
+        this.expandKey = [this.category.parentCid];
+        this.getMenus();
+      });
+    },
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   created() {
-      this.getMenus();
-  }
+    this.getMenus();
+  },
 };
 </script>
 
